6a677a6bc8d5fa8d81c14b52c09e69b4839501d5
ANDRYSKA <fylh.irf174@mail.ru>
2022-12-28 18:34:26 +0400

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int sum = 20;
+        int result = price / sum;
+        return result;
+
+    }
+}
+
+
---
+++ src/Main4.java
@@ -0,0 +1,8 @@
+public class Main4 {public static void main(String[] args) {
+    BonusMilesService service = new BonusMilesService();
+    int price = 10_000;
+    int miles = service.calculate(price);
+    System.out.println(miles);
+}
+}
+
